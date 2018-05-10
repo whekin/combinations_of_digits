b0e4b5c51b74920a8e20fd7af8f115d1d5c0640f
Stanislav <39103584+whekin@users.noreply.github.com>
2018-05-11 00:31:39 +0300

    added comments

--- combinations.pas
+++ combinations.pas
@@ -1,7 +1,9 @@
-// num -> digits ->shuffle
+// num -> digits -> shuffle
+
 type
     dynamic_array = array of integer;
     
+// функция получает число и возвращает массив цифр этого числа в обратном порядке, например 123 -> [3, 2, 1]
 function getDigitsOfNum(num: integer): dynamic_array;
 var digits: array of integer;
     a, n, i: integer;
@@ -18,6 +20,7 @@ begin
   getDigitsOfNum := digits;
 end;
 
+// функций возвращает факториал числа, например - 5 -> 5! (120)
 function factorial(num: integer): integer;
 begin
     if num = 1 then begin
@@ -26,6 +29,7 @@ begin
         factorial := num * factorial(num - 1);
 end;
 
+// функция обьединят числа массива в число и возвращает его, например - [1,2,3] -> 123
 function arrToInt(ar: dynamic_array): integer;
 var
     len: integer;
@@ -39,27 +43,30 @@ begin
     arrToInt := a;
 end;
 
+// Функция на вход получает число и возвращает в массиве все комбинации из чисел в этом числе, например - 123 -> [231, 213, 123, 132, 312, 321]
 function shuffle(num: integer): dynamic_array;
 var digits, shuffled, d: dynamic_array;
     amount, c, len, a, b: integer;
     
 begin
-  digits := getDigitsOfNum(num);
-  len := digits.length;
-  amount := factorial(len);
-  setLength(shuffled, amount);
-  d := digits;
+  digits := getDigitsOfNum(num); // получаем массив чисел из числа
+  len := digits.length; // длина числа (количество цифр)
+  amount := factorial(len); // количество возможных комбинаций из числа
+  setLength(shuffled, amount); // задаем массиву с комбинациями длину равную количеству комбинаций
+  d := digits; // копируем цифры
   
+  // основной алгоритм
   for var i := 0 to amount div (len - 1) - 1 do
   begin
     for var j := 0 to len - 2 do
     begin
-        a := d[j];
-        b := d[j + 1];
-        d[j] := b;
-        d[j + 1] := a;
-        shuffled[c] := arrToInt(d);
-        inc(c);
+        a := d[j];    // |
+        b := d[j + 1];// < меняем два числа местами в массиве
+        d[j] := b;    // |
+        d[j + 1] := a;// |
+        
+        shuffled[c] := arrToInt(d); // записываем в массив комбинаций новую комбинацию
+        inc(c); 
     end;
   end;
  shuffle := shuffled;
@@ -70,7 +77,7 @@ var
     num: integer;
 begin
     readln(num);
-    array_of_shuffled_digits :=shuffle(num);
+    array_of_shuffled_digits := shuffle(num);
     for var i := 0 to array_of_shuffled_digits.length - 1 do
         write(array_of_shuffled_digits[i], ' ');
 end.
